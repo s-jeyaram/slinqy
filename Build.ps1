1c62f167aa4ee68742add3f722e3d07bba627c27
Matthew Thomas <matthew.thomas@rakuten.com>
2015-09-18 14:32:28 -0700

    Sets up initial structure for CI scripts.

--- Build.ps1
+++
@@ -1,9 +0,0 @@
-# Install Chocolately if it's not already
-if (-not ($env:Path -ilike "*chocolatey*")) {
-    Write-Host "Chocolatey not found in PATH environment variable, installing..."
-    
-    iex ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-}
-
-# Make sure all other required dependencies are installed...
-cinst psake --version 4.4.1 --confirm
---
+++ Scripts/CI.Core.ps1
@@ -0,0 +1,5 @@
+Task Default -depends Build
+
+Task Build {
+   Write-Output 'Hello, World from the Build task!'
+}
