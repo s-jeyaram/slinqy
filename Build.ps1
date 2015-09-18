96a01ab9a3216c7018236eb01ce505528f952aba
Matthew Thomas <matthew.thomas@rakuten.com>
2015-09-18 13:25:23 -0700

    Modifies the build script to auto install Chocolately.

--- Build.ps1
+++ Build.ps1
@@ -1,1 +1,6 @@
-Write-Host 'Hello, World from Build.ps1!'
+# Install Chocolately if it's not already
+if (-not ($env:Path -ilike "*chocolatey*")) {
+    Write-Host "Chocolatey not found in PATH environment variable, installing..."
+    
+    iex ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
