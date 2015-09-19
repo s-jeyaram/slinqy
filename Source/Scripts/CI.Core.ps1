c541cb0e3e6c09b8a7ccae27ea54e846b8455ca5
Matthew Thomas <matthew.thomas@rakuten.com>
2015-09-18 17:41:13 -0700

    Modifies the CI scripts to add a new Clean task.  Also modifies the AppVeyor config to capture CI log artifacts.

--- Source/Scripts/CI.Core.ps1
+++ Source/Scripts/CI.Core.ps1
@@ -10,13 +10,19 @@ properties {
 # Define the Task to call when none was specified by the caller.
 Task Default -depends Build
 
+Task Clean -description "Removes any artifacts that may be present from prior runs of the CI script." {
+	if (Test-Path $ArtifactsPath) {
+		Remove-Item $ArtifactsPath -Recurse -Force
+	}
+}
+
 Task InstallDependencies -description "Installs all dependencies required to execute the tasks in this script." {
 	exec { 
 		cinst invokemsbuild --version 1.5.17 --confirm
 	}
 }
 
-Task Build -depends InstallDependencies -description "Compiles all source code." {
+Task Build -depends Clean,InstallDependencies -description "Compiles all source code." {
 	$SolutionPath = Join-Path $SourcePath "Slinqy.sln"
 
 	Write-Host "Building $SolutionPath"
