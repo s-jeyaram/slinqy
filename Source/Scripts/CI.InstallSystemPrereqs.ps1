723973198f259ab0a06532562a2568979d385093
Matthew Thomas <matthew.thomas@rakuten.com>
2015-10-07 12:01:37 -0700

    Adds a little more commentary to the CI.InstallSystemPrereqs script.

--- Source/Scripts/CI.InstallSystemPrereqs.ps1
+++ Source/Scripts/CI.InstallSystemPrereqs.ps1
@@ -1,12 +1,18 @@
 #requires -RunAsAdministrator
 
-# Make sure all the system level prerequisites that require Admin rights are installed
+# Make sure all the system level prerequisites that require Admin rights are installed.
+
 # Chocolately: Used to install subsequent dependencies.
 if (-not ($env:Path -ilike "*chocolatey*")) {
     Write-Host "Chocolatey not found in PATH environment variable, installing..."
     iex ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
+# PowerShell is used to run scripts.
 cinst powershell --version 5.0.10514-ProductionPreview --confirm
+
+# .NET 4.6 is the Framework that compiles and runs compiled code.
 cinst dotnet4.6  --version 4.6.00081.20150925          --confirm
+
+# PSake is a tool that coordinates CI tasks.
 cinst psake      --version 4.4.1                       --confirm
