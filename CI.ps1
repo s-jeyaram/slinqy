4d3f197c6cf3fa02d7b20feece7e68d4dcc4622e
Matthew Thomas <matthew.thomas@rakuten.com>
2015-10-16 17:47:17 -0700

    Modifies the CI scripts to check the last exit code after the psake exe runs so that the build can fail if psake reports an error.

--- CI.ps1
+++ CI.ps1
@@ -1,1 +1,5 @@
 psake .\Source\Scripts\CI.Tasks.ps1 $args -parameters "@{BasePath='$PSScriptRoot'}" -nologo
+
+if ($LASTEXITCODE -ne 0) {
+    throw "PSake failed with exit code $LASTEXITCODE.  See previous errors for details."
+}
