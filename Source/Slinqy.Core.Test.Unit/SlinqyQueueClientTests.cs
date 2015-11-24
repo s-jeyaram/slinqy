2827b87b8626c3a62381dea0bc972b92934d83f1
Matthew Thomas <matthew.thomas@rakuten.com>
2015-11-23 16:37:27 -0800

    Adds unit test Get_SlinqyQueueDoesNotExistInCollection_IsReturned and refactors how its parent class handles instantiating the client.

--- Source/Slinqy.Core.Test.Unit/SlinqyQueueClientTests.cs
+++ Source/Slinqy.Core.Test.Unit/SlinqyQueueClientTests.cs
@@ -10,7 +10,17 @@
     /// </summary>
     public class SlinqyQueueClientTests
     {
+        private readonly SlinqyQueueClient client;
         private readonly IPhysicalQueueService fakePhysicalQueueService = A.Fake<IPhysicalQueueService>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SlinqyQueueClientTests"/> class.
+        /// </summary>
+        public
+        SlinqyQueueClientTests()
+        {
+            this.client = new SlinqyQueueClient(this.fakePhysicalQueueService);
+        }
 
         /// <summary>
         /// Verifies the constructor checks for null values.
@@ -34,10 +44,10 @@
         CreateAsync_QueueNameValid_CreateDelegateInvoked()
         {
             // Arrange
-            var client = new SlinqyQueueClient(this.fakePhysicalQueueService);
+            const string queueName = "test";
 
             // Act
-            await client.CreateAsync("test");
+            await this.client.CreateAsync(queueName);
 
             // Assert
             A.CallTo(() =>
@@ -57,13 +67,29 @@
             // Arrange
             string queueName = null;
 
-            var client = new SlinqyQueueClient(
-                this.fakePhysicalQueueService
-            );
+            // Act
+            Func<Task<SlinqyQueue>> action = async () => await this.client.CreateAsync(queueName);
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(action);
+        }
+
+        /// <summary>
+        /// Verifies that a SlinqyQueue instance is added and returned when one doesn't already exist.
+        /// </summary>
+        [Fact]
+        public
+        void
+        Get_SlinqyQueueDoesNotExistInCollection_IsReturned()
+        {
+            // Arrange
+            const string queueName = "test";
 
             // Act
+            var actualQueueName = this.client.Get(queueName).Name;
+
             // Assert
-            await Assert.ThrowsAsync<ArgumentNullException>(async () => await client.CreateAsync(queueName));
+            Assert.Equal(queueName, actualQueueName);
         }
     }
 }
