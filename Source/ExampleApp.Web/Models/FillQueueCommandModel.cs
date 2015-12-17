5e69ca65a414b7478ec95f16249f33198ad9e570
Matthew Thomas <matthew.thomas@rakuten.com>
2015-12-16 20:39:16 -0800

    Modifies the Example App to allow the user to specify how much data to fill the queue will instead of it just running forever.

---
+++ Source/ExampleApp.Web/Models/FillQueueCommandModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ExampleApp.Web.Models
+{
+    /// <summary>
+    /// Specifies the required parameters to fill a queue.
+    /// </summary>
+    public class FillQueueCommandModel
+    {
+        /// <summary>
+        /// Gets or sets the amount of data, in megabytes, that should be sent to the queue.
+        /// </summary>
+        public int SizeMegabytes { get; set; }
+    }
+}
