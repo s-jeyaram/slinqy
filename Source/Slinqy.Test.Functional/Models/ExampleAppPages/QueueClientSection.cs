6c02c432eb36b7dcd981c65297d457e97f546a1f
Matthew Thomas <matthew.thomas@rakuten.com>
2015-12-30 15:07:29 -0800

    Setting the wait time back to a more appropriate value.

--- Source/Slinqy.Test.Functional/Models/ExampleAppPages/QueueClientSection.cs
+++ Source/Slinqy.Test.Functional/Models/ExampleAppPages/QueueClientSection.cs
@@ -138,7 +138,7 @@
                 from:           () => this.receiveQueueButton.Enabled,
                 until:          enabled => enabled,
                 interval:       TimeSpan.FromMilliseconds(500),
-                maxDuration:    TimeSpan.FromSeconds(15)
+                maxDuration:    TimeSpan.FromSeconds(30)
             );
 
             // Get the # of messages from the UI.
