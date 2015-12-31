6e364a18a4964ba115d084c4c60a2fdcf3c2c089
Matthew Thomas <matthew.thomas@rakuten.com>
2015-12-30 17:41:27 -0800

    Also updates the timeout for waiting on queue depletion from 30 to 60.

--- Source/Slinqy.Test.Functional/Models/ExampleAppPages/QueueClientSection.cs
+++ Source/Slinqy.Test.Functional/Models/ExampleAppPages/QueueClientSection.cs
@@ -138,7 +138,7 @@
                 from:           () => this.receiveQueueButton.Enabled,
                 until:          enabled => enabled,
                 interval:       TimeSpan.FromMilliseconds(500),
-                maxDuration:    TimeSpan.FromSeconds(30)
+                maxDuration:    TimeSpan.FromSeconds(60)
             );
 
             // Get the # of messages from the UI.
