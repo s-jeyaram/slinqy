ae7a13991249298aac56d9db272987884bd20068
Matthew Thomas <matthew.thomas@rakuten.com>
2016-01-08 22:32:32 -0800

    Modifies the views to use glyphicons to represent the AJAX status.

--- Source/ExampleApp.Web/Scripts/ajax.indicator.js
+++ Source/ExampleApp.Web/Scripts/ajax.indicator.js
@@ -4,41 +4,50 @@ ajax = function () {
     // Called when an AJAX request succeeds.
     function onAjaxRequestSucceeded(statusElement) {
         statusElement
+            .addClass('glyphicon-ok-circle')
             .addClass('ajax-succeeded')
-            .text('$')
             .attr('title', 'AJAX request succeeded!');
     }
 
     // Called when an AJAX request fails.
-    function onAjaxRequestFailed(request, type, message, statusElement) {
+    function onAjaxRequestFailed(request, type, message, statusElement, autoShowError) {
+        var failedText = 'Failed : ' + request.status + ' : ' + type + ' : ' + message + ' : ' + request.responseText;
+
         statusElement
+            .addClass('glyphicon-remove-circle')
             .addClass('ajax-failed')
-            .text('Failed : ' + request.status + ' : ' + type + ' : ' + message + ' : ' + request.responseText);
+            .attr('title', failedText);
+
+        if (autoShowError) {
+            statusElement.text(failedText);
+        }
     }
 
     // Called when an aJAX request finished, regardless if it was successful or not.
     function onAjaxRequestCompleted(statusElement) {
         statusElement
             .addClass('ajax-completed')
-            .removeClass('ajax-running');
+            .removeClass('ajax-running')
+            .removeClass('glyphicon-circle-arrow-right');
     }
 
     // Registers an AJAX request to be monitored and reported.
-    // ajaxRequest: A currently executing AJAX request.
-    // statusElement: a jQuery element that should be used to display the status of the AJAX request to the user.
-    function indicate(ajaxRequest, statusElement) {
+    // ajaxRequest:   A currently executing AJAX request.
+    // statusElement: A jQuery element that should be used to display the status of the AJAX request to the user.
+    // autoShowError: A boolean value indicating whether error message content will be displayed (true) or require mouse-hover (false).
+    function indicate(ajaxRequest, statusElement, autoShowError) {
         // First update the status element to indicate to the user that the request is running.
         statusElement
-            .text('>')
-            .attr('class', 'ajax-running') // reset the class attribute
-            .attr('title', 'Request submitting, awaiting a response from the server.');
+            .attr('class', 'glyphicon glyphicon-circle-arrow-right ajax-running')
+            .attr('title', 'Request submitting, awaiting a response from the server.')
+            .text('');
 
         // Register event handlers on the request.
         ajaxRequest
             .done(function()   { onAjaxRequestSucceeded(statusElement); })
             .always(function() { onAjaxRequestCompleted(statusElement); })
             .fail(function(request, type, message) {
-                 onAjaxRequestFailed(request, type, message, statusElement);
+                onAjaxRequestFailed(request, type, message, statusElement, autoShowError);
             });
     }
 
