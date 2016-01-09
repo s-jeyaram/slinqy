3bceb6083111500adb8298868619904a9fb8bf4b
Matthew Thomas <matthew.thomas@rakuten.com>
2016-01-08 18:37:17 -0800

    Adds a re-usable widget for reporting status of AJAX operations to the user.

--- Source/ExampleApp.Web/App_Start/BundleConfig.cs
+++ Source/ExampleApp.Web/App_Start/BundleConfig.cs
@@ -33,6 +33,7 @@
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/slinqy").Include("~/Scripts/ajax.indicator.js"));
         }
     }
 }
---
+++ Source/ExampleApp.Web/Scripts/ajax.indicator.js
@@ -0,0 +1,49 @@
+﻿// Adds an ajax namespace with functions for updating UI depending on AJAX operations.
+
+ajax = function () {
+    // Called when an AJAX request succeeds.
+    function onAjaxRequestSucceeded(statusElement) {
+        statusElement
+            .addClass('ajax-succeeded')
+            .text('$')
+            .attr('title', 'AJAX request succeeded!');
+    }
+
+    // Called when an AJAX request fails.
+    function onAjaxRequestFailed(request, type, message, statusElement) {
+        statusElement
+            .addClass('ajax-failed')
+            .text('Failed : ' + request.status + ' : ' + type + ' : ' + message + ' : ' + request.responseText);
+    }
+
+    // Called when an aJAX request finished, regardless if it was successful or not.
+    function onAjaxRequestCompleted(statusElement) {
+        statusElement
+            .addClass('ajax-completed')
+            .removeClass('ajax-running');
+    }
+
+    // Registers an AJAX request to be monitored and reported.
+    // ajaxRequest: A currently executing AJAX request.
+    // statusElement: a jQuery element that should be used to display the status of the AJAX request to the user.
+    function indicate(ajaxRequest, statusElement) {
+        // First update the status element to indicate to the user that the request is running.
+        statusElement
+            .text('>')
+            .attr('class', 'ajax-running') // reset the class attribute
+            .attr('title', 'Request submitting, awaiting a response from the server.');
+
+        // Register event handlers on the request.
+        ajaxRequest
+            .done(function()   { onAjaxRequestSucceeded(statusElement); })
+            .always(function() { onAjaxRequestCompleted(statusElement); })
+            .fail(function(request, type, message) {
+                 onAjaxRequestFailed(request, type, message, statusElement);
+            });
+    }
+
+    return {
+        // Define what gets exported in the parent namespace
+        indicate:indicate
+    }
+}();
